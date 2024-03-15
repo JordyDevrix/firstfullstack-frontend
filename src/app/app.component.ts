f5251a41aecc3764452b716add576e351c42e6ee
Admiraal <florisadmiraal@gmail.com>
2024-03-15 13:52:34 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AuthModule, CoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UnprotectedComponent } from './unprotected/unprotected.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { authGuard } from './auth/auth.guard';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'auth/login', component: LoginComponent },
+    {path: 'auth/register', component: RegisterComponent},
+    
+    {path: 'unprotected', component: UnprotectedComponent },
+    {path: 'protected', component: ProtectedComponent, canActivate: [authGuard] },
+    
+];
